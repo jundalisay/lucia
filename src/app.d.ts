2c33f4822336efc59d8e21d893588585385d3c4e
“jundalisay” <jundalisay@yahoo.com>
2024-10-12 12:21:50 +0800

    update

--- src/app.d.ts
+++ src/app.d.ts
@@ -9,6 +9,17 @@ declare global {
 			user: import('lucia').User | null;
 			session: import('lucia').Session | null;
 		}
+
+		interface Platform {
+		       env: {
+		           COUNTER: DurableObjectNamespace;
+		       };
+		       context: {
+		           waitUntil(promise: Promise<any>): void;
+		       };
+		       caches: CacheStorage & { default: Cache }
+		   }
+
 		interface PageData {
 			pageMetaTags?: MetaTagsProps;
 			isUserLoggedIn: boolean;
